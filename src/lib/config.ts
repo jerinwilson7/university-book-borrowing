c484d158ba256be749c4553016f77485129e5785
JerinWilson <jerin.wilson@vonnue.com>
2025-02-12 11:49:23 +0530

    qstash workflow

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   env: {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
-    prodApiEndpoint: process.env.PROD_API_ENDPOINT!,
+    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     databaseUrl: process.env.DATABASE_URL!,
     resendToken: process.env.RESEND_TOKEN!,
     imageKit: {
