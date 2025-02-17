1726a33041cc4d7167ca8d1fc20bb53230e324fa
JerinWilson <jerin.wilson@vonnue.com>
2025-02-17 12:13:55 +0530

    refactor: header

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,8 +1,9 @@
+import AdminLogo from "./admin/logo.svg";
 import Book from "./book.svg";
 import Calender from "./calendar.svg";
 import Logo from "./logo.svg";
 import Star from "./star.svg";
 import Upload from "./upload.svg";
-import AdminLogo from "./admin/logo.svg"
 
-export { Book, Calender, Logo, Star, Upload, AdminLogo };
+export { AdminLogo, Book, Calender, Logo, Star, Upload };
+
