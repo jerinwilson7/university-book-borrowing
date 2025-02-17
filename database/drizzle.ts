def22ed182e1861db9c78d87a8efb4d275410bb9
JerinWilson <jerin.wilson@vonnue.com>
2025-02-17 10:08:57 +0530

    feat: neon db string

--- database/drizzle.ts
+++ database/drizzle.ts
@@ -1,6 +1,10 @@
 import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
 
+config({ path: ".env.local" });
 
-const sql = neon(process.env.DATABASE_URL!);
+console.log("DB", process.env.NEXT_PUBLIC_DATABASE_URL!);
+
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL!);
 export const db = drizzle({ client: sql });
